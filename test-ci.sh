2cad1377c5d9b5c9841304b1af8cdbc5948d63af
Jordan Williams <jordan@jwillikers.com>
2021-06-19 13:24:51 -0500

    Add fuse device

--- test-ci.sh
+++ test-ci.sh
@@ -2,10 +2,10 @@
 set -o errexit
 
 echo "Compiling test program"
-podman run --rm --volume "$PWD":/home/user --name test-container -t localhost/gcc g++ test/main.cpp
+podman run --rm --device /dev/fuse --volume "$PWD":/home/user --name test-container -t localhost/gcc g++ test/main.cpp
 
 echo "Running test program"
-podman run --rm --volume "$PWD":/home/user --name test-container -t localhost/gcc ./a.out
+podman run --rm --device /dev/fuse --volume "$PWD":/home/user --name test-container -t localhost/gcc ./a.out
 
 echo "Cleaning up"
 rm a.out
