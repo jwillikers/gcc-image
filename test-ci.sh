73bb21c38ea29ffb4b143357c04a02f2617f3941
Jordan Williams <jordan@jwillikers.com>
2021-06-19 12:10:50 -0500

    Run the CI script using podman unshare

--- test-ci.sh
+++ test-ci.sh
@@ -2,10 +2,10 @@
 set -o errexit
 
 echo "Compiling test program"
-sudo podman run --rm --volume "$PWD":/home/user --name test-container -t localhost/gcc g++ test/main.cpp
+podman run --rm --volume "$PWD":/home/user --name test-container -t localhost/gcc g++ test/main.cpp
 
 echo "Running test program"
-sudo podman run --rm --volume "$PWD":/home/user --name test-container -t localhost/gcc ./a.out
+podman run --rm --volume "$PWD":/home/user --name test-container -t localhost/gcc ./a.out
 
 echo "Cleaning up"
 rm a.out
