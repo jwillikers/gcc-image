df28a8489b1e1433f0fec25e97a9fb1673cd1461
Jordan Williams <jordan@jwillikers.com>
2021-06-19 10:39:47 -0500

    Add printouts to the test script

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Compiling test program"
 podman run --userns keep-id --rm --volume "$PWD":/home/user:Z --name test-container localhost/gcc g++ test/main.cpp
+
+echo "Running test program"
 podman run --userns keep-id --rm --volume "$PWD":/home/user:Z --name test-container localhost/gcc ./a.out
+
+echo "Cleaning up"
 rm a.out
