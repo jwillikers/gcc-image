633eb57f4fe5b855157c1e4c58cb0fa145153e6e
Jordan Williams <jordan@jwillikers.com>
2021-06-19 10:50:36 -0500

    Remove user namespace option for CI

--- test-ci.sh
+++ test-ci.sh
@@ -2,10 +2,10 @@
 set -o errexit
 
 echo "Compiling test program"
-podman run --userns keep-id --security-opt label=disable --rm --volume "$PWD":/home/user --name test-container localhost/gcc g++ test/main.cpp
+podman run --security-opt label=disable --rm --volume "$PWD":/home/user --name test-container localhost/gcc g++ test/main.cpp
 
 echo "Running test program"
-podman run --userns keep-id --security-opt label=disable --rm --volume "$PWD":/home/user --name test-container localhost/gcc ./a.out
+podman run --security-opt label=disable --rm --volume "$PWD":/home/user --name test-container localhost/gcc ./a.out
 
 echo "Cleaning up"
 rm a.out
